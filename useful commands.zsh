70a32b9e4c06cac3b8a04304d6ab0901413f86eb
dom <dev@gedon.org>
2016-09-08 11:42:44 +0200

    Update and rename useful commands to useful commands.zsh

--- useful commands.zsh
+++ useful commands.zsh
@@ -1,4 +1,4 @@
-```Shell
+
 
 # mount samba share
   sudo mount -t cifs //name/share /mnt -o username=username
@@ -9,5 +9,3 @@
   
 # Put the drive into idle (low-power) mode, and also set the standby (spindown) timeout for the drive.
   hdparm -S 60 /dev/sdc
-  
-```
