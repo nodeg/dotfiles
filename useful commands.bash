b7cd1da49d72b4a2931b3df5217de2f8433a9110
dom <dev@gedon.org>
2016-09-08 11:44:38 +0200

    Update useful commands.bash

--- useful commands.bash
+++ useful commands.bash
@@ -1,4 +1,3 @@
-```bash
 
 # mount samba share
   sudo mount -t cifs //name/share /mnt -o username=username
@@ -9,4 +8,3 @@
   
 # Put the drive into idle (low-power) mode, and also set the standby (spindown) timeout for the drive.
   hdparm -S 60 /dev/sdc
-```
