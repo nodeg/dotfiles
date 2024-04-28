bad01c9d4ab8ecff8d5786db2155432353a2cbf2
Dominik Gedon <dominik.gedon@suse.com>
2024-04-28 19:54:46 +0200

    [wezterm] Update config for Neovim

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
+local mux = wezterm.mux
 
 -- This table will hold the configuration.
 local config = {}
@@ -23,7 +24,39 @@ config.hide_mouse_cursor_when_typing = false
 -- https://wezfurlong.org/wezterm/config/lua/config/term.html
 config.term = 'xterm-256color'
 
+config.automatically_reload_config = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
+-- always start maximised
+wezterm.on('gui-startup', function(cmd)
+    local tab, pane, window = mux.spawn_window(cmd or {})
+    window:gui_window():maximize()
+  end)
+
+-- necessary for https://github.com/folke/zen-mode.nvim
+wezterm.on('user-var-changed', function(window, pane, name, value)
+  local overrides = window:get_config_overrides() or {}
+    if name == "ZEN_MODE" then
+        local incremental = value:find("+")
+        local number_value = tonumber(value)
+        if incremental ~= nil then
+            while (number_value > 0) do
+                window:perform_action(wezterm.action.IncreaseFontSize, pane)
+                number_value = number_value - 1
+            end
+            overrides.enable_tab_bar = false
+        elseif number_value < 0 then
+            window:perform_action(wezterm.action.ResetFontSize, pane)
+            overrides.font_size = nil
+            overrides.enable_tab_bar = true
+        else
+            overrides.font_size = number_value
+            overrides.enable_tab_bar = false
+        end
+    end
+    window:set_config_overrides(overrides)
+end)
 
 -- and finally, return the configuration to wezterm
 return config
